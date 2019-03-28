cd8cdd58a8374f28f97d5525681a9fb4a93706af
GWOLLENBE001 <gwollenbe001@vplincsci027.ad.csbsju.edu>
2019-03-28 08:03:15 -0500

    yoink

---
+++ src/edu/csbsju/ntc/NewTelephoneCompanyTest_WBT.java
@@ -0,0 +1,42 @@
+package edu.csbsju.ntc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NewTelephoneCompanyTest_WBT {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void testComputeChargeInvalidStartTime() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testComputeChargeInvalidDuration() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testComputeChargeDiscountedStartAndDiscountedDuration() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testComputeChargeDiscountedStartOnly() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testComputeChargeDiscountedDurationOnly() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testComputeChargeNoDiscount() {
+		fail("Not yet implemented");
+	}
+
+
+}
